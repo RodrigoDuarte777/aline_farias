7bbbb9e2a0bc391537243670fcf82a78da33ff43
RodrigoDuarte777 <duartesolutions2022@gmail.com>
2023-01-31 20:34:09 -0300

    first commit

---
+++ Aline fisio/index.php
@@ -0,0 +1,220 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicial</title>
+    <link rel="stylesheet"  href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="icon" href="logoa.png" type="image/x-icon" />
+
+    <link href="css/business-frontpage.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+</head>
+
+<body style="background-color: black; color: white;" >
+    
+   
+   
+
+    <div class="pos-f-t" style="color:white;background-color: black">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="p-4" style="background-color: black;color:white;">
+            <h5 class="text-white h4">Aline Farias<mark style="color: yellow;background-color: black">personal</mark></h5>
+            <a class="nav-link" href="Aline fisiooo/espaco.html" style="color: white">Espaço fitness</a>
+         
+         <a class="nav-link" href="Antes e depois/antesdepois.html"style="color: white">Antes e depois</a>
+          </div>
+        </div>
+        <nav class="navbar navbar-dark " style="background-color: black">
+                <img src="logobranca (1).png" alt="Logo" style="width: 60px;height: 40px;">
+            
+                <h6> Aline Farias<mark style="color: yellow ;background-color: black">personal</mark></h6>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon">
+               
+            </span>
+          </button>
+        </nav>
+      </div>
+                         <!banner->
+                         
+                         <div class="container-fluid">
+               <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                   <div class="row">
+                       <div class="col-sm">
+               <img class="d-block w-100" src="espacologo.jpeg" alt=""style="width: 600px;" >
+                   </div>
+                  </div>
+                    </div>
+                    </div>
+                    <br>
+                    <br>
+                    <div class="container-fluid">
+                    <div class="row">
+                    <div class="container-fluid">
+                    
+                        <center> <h2>O que você<mark style="color: yellow ;background-color: black">vai aprender:</mark></h2></center>
+                        <br>
+                      <div>
+                        <img src="certo.png" alt="" style="width: 50px;height: 50px;" align="left"><h5 style="text-align: justify;"> Treinamento funcional, onde treina padrões de movimentos naturais do ser humano
+                        como: Pular, agachar, empurrar, puxar, correr, etc. Incuidos nesses padrões estão 4 pilares para montar o treino.</h5>
+                        <br><center><a href=" espaco.html #funci"><button class="btn" style="background-color: yellow;">saiba mais</button></a></center>
+                    </div>
+                    <br>
+                    <div>
+                        <img src="certo.png" alt="" style="width: 50px;height: 50px;" align="left"><h5 style="text-align: justify;">Natação. Atividade fisica excelente para o coração, fortalece os pulmões, melhora condicionamento físico
+                            emagrece e previne diabetes.</h5>
+                        <br>
+                        <center><a href=" espaco.html #natacao"><button class="btn" style="background-color: yellow;">saiba mais</button></a></center>
+                    </div>
+                    <br>
+                    <div>
+                        <img src="certo.png" alt="" style="width: 50px;height: 50px;" align="left"><h5 style="text-align: justify;">hidroginástica. Vai te ajudar a sair do sedentarismo, e ajudara perder peso, além de melhorar o condicionamento físico.</h5>
+                        <br><center><a href=" espaco.html #hidro"><button class="btn" style="background-color: yellow;">saiba mais</button></a></center>
+                    </div>
+                    
+                    <br>
+                    <br>
+                    </div>
+                    <div class="container-fluid" style="background-color: white; color: black; ">
+                        <br>
+                        <center> <h3>O treinamento<mark style="color: yellow ;background-color: white">inclui:</mark></h3></center>
+                        <br>
+                        
+                    <center><img src="casa.png" alt="" style="width: 300px; height: 300px;"></center>
+               
+                <center>treine em casa ou academia</center>
+                <div class="modal fade" id="casademia" tabindex="-1" role="dialog" aria-labelledby="casademia" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="casademia">Sobre</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					
+				</button>
+			</div>
+			<div class="modal-body">
+				<img style="width: 320px; height: 250px; " src="casa.jpg"><br>
+                Protocolos personalizados, de acordo com o ambiente que você quer treinar: Em casa ou em uma academia
+               . Você irá preencher um formulário para que eu possa te conhecer melhor e saber seus objetivos e limitações! Depois de ler e avaliar todo teu histórico montarei um treino dentro das tuas necessidades/possibilidades
+               e que te proporcione o melhor resultado possível.</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+			</div>
+		</div>
+    </div>
+    
+</div>
+                <br><center><a data-toggle="modal" data-target="#casademia"><button class="btn btn-lg" style="background-color:yellow">saiba mais</button></a></center>
+            
+                    <br>
+                    <br>   
+                    <center><img src="monitor.png" alt="" style="width: 200px; height: 200px;"></center>
+               
+                <center>aulas online</center>
+                <br> <div class="modal fade" id="online" tabindex="-1" role="dialog" aria-labelledby="online" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="online">Sobre</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					
+				</button>
+			</div>
+			<div class="modal-body">
+                <img style="width: 320px; height: 250px; " src="online.jpg"><br>
+                Aulas online, para auxiliar os treinos com consultoria com aulas gravadas via telegram
+                ideoconferências comuns, no Zoom, criando salas privadas e convidando os alunos para ingressarem 
+                no horário combinado. Em todas as opções, o docente pode usar slides, vídeos, páginas da Internet e demais recursos audiovisuais para tornar a aula mais dinâmica.
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+			</div>
+		</div>
+    </div>
+    
+</div>
+                <br><center><a data-toggle="modal" data-target="#online"><button class="btn btn-lg" style="background-color:yellow">saiba mais</button></a></center>
+            
+                <br>   
+                    <center><img src="central.png" alt="" style="width: 200px; height: 200px;"></center>
+                
+                <center>Suporte</center>
+                <br> <div class="modal fade" id="suporte" tabindex="-1" role="dialog" aria-labelledby="suporte" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="suporte">Sobre</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					
+				</button>
+			</div>
+			<div class="modal-body">
+				<img style="width: 320px; height: 250px; " src="suporte.jpg"><br>
+                Com o suporte ocorre soluções rápida e eficiente para qualquer problema que possa surgir. Os problemas e panes são resolvidos imediatamente.
+            </div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+			</div>
+		</div>
+    </div>
+    
+</div>
+                <br><center><a data-toggle="modal" data-target="#suporte"><button class="btn btn-lg" style="background-color:yellow">saiba mais</button></a></center>
+            
+                    <br>
+                    </div>
+                    <!quem é aline->
+                    <div class="container" style="text-align:justify;font: italic; height=100%" align="center" >
+                       
+                        <center><img src="alinequem.png" alt="" style="width: 400px;height:300px;">
+                           <br>
+                           <br>
+                        <h1>Quem é<mark style="color: yellow ;background-color: black">Aline Farias</mark> </h1>
+                       </center>
+             <center>        
+                      "Profissional de Educação Física formada pela Universidade Vale do Acaraú(UVA), e Especialista em personal Trainer pela faculdade Darcy Ribeiro.
+               Vários cursos na área de Treinamento Funcional,musculação,hidroginástica e Natação. Já atuou como professora no projeto Segundo Tempo. Foi praticante de Kung
+               por 8 anos. é Proprietária e Instrutora do Espaço Fitness Aline Farias, o qual tem modalidades: Treinamento Funcional, Hidroginástica e Natação."
+               </center> 
+                       
+                       <br>
+                       <br>
+                       </div>
+                       <div class="container-fluid" style="background-color: white;color: black;text-align:justify;font: italic">
+                           <br>
+               <center>  <h3>  Como funciona a<mark style="color: yellow ;background-color: white;">minha consultoria</mark> </h3> </center>
+               <br>
+               <center>  Na minha consultoria utilizo de protocolos personalizados, de acordo com o ambiente que você quer treinar: Em casa ou em uma academia
+               . Você irá preencher um formulário para que eu possa te conhecer melhor e saber seus objetivos e limitações! Depois de ler e avaliar todo teu histórico montarei um treino dentro das tuas necessidades/possibilidades
+               e que te proporcione o melhor resultado possível. Daí você receberá essee treino via e-mail junto a um manual explicativo. Receberá um contato via whatsapp que permite um contato frequente(dias e horários comerciais)
+               além da possibilidade de utilizar o     e-mail: <strong>alinefariaspersonal@hotmail.com</strong>. A duração de cada programa é de 30 dias e esse é o período que estou a sua disposição para sanar dúvidas e poder te auxiliar da melhor maneira
+               possível para que obtenha excelentes resultados.</center>
+<br>
+                       </div>
+                       <br>
+                     </div>
+                     </div> 
+                     <nav class="rodape " style="position: absolute;"></nav><footer class="py-5" style="background-color:black;color:white">
+                     <div class="container">
+                     <p class="m-0 text-center text-white">© 2020 Copyright - AlineFarias</p>
+                     <img src="whats.png" alt="Logo" style="width: 30px;height: 30px;">
+                     <br>
+                    <img src="insta.png" alt="Logo" style="width: 30px;height: 30px;">
+                    <br>
+                    <img src="telegram.png" alt="Logo" style="width: 30px;height: 30px;">
+<br>
+<img src="youtube.png" alt="Logo" style="width: 30px;height: 30px;">
+
+                      </div>
+  
+  </footer>
+            
+       
+
+    <script src=" vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ 
+</body>
+</html>
